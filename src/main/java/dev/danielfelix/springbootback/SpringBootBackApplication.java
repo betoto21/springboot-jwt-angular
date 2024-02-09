eeb7ae327dca0baee2fb2bbb4d43519ac5238659
Daniel Felix <danielcoo.dafb@gmail.com>
2024-02-09 15:16:58 -0700

    created aplication.properties devconfig

---
+++ src/main/java/dev/danielfelix/springbootback/SpringBootBackApplication.java
@@ -0,0 +1,13 @@
+package dev.danielfelix.springbootback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackApplication.class, args);
+    }
+
+}
