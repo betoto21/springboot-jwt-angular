d45f80140248cb2b1c8c406bcd1a5a70c8dcf5aa
Daniel Felix <danielcoo.dafb@gmail.com>
2024-02-09 15:16:58 -0700

    created aplication.properties devconfig

---
+++ src/test/java/dev/danielfelix/springbootback/SpringBootBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danielfelix.springbootback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
